a60f63c8b9eacc0475f3babc2fd1c2060b67602b
Tejal Bangali <63381263+tejalbangali@users.noreply.github.com>
2021-04-23 17:29:26 +0530

    Sum Of Two Numbers Given By User

--- Sum of two numbers.py
+++ Sum of two numbers.py
@@ -1,3 +1,3 @@
-num1=int(input('Enter 1st number: '))
+num1=int(input('Enter 1st number: '))   # Taking input from user and converting it into Integer
 num2=int(input('Enter 2nd number: '))
-print('Sum of both numbers is:',num1+num2)
+print('Sum of both numbers is:',num1+num2)  # Printing Sum of both numbers
