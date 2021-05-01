8b6cb347ad6ce0707e640fbebe894009f576f99b
Tejal Bangali <63381263+tejalbangali@users.noreply.github.com>
2021-05-01 23:51:22 +0530

    Convert_Temperature

---
+++ Temperature Conversion.py
@@ -0,0 +1,8 @@
+#--> Write a program to convert temperatures from Celsius to Fahrenheit.
+# -> Input Format
+#       60 (in C)
+# -> Output Format
+#       140( in F)
+
+tem=int(input())
+print(int((tem*9/5)+32))
