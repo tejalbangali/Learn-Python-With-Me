19805fc81882ee5796c57efdf610cca4585ab54f
Tejal Bangali <63381263+tejalbangali@users.noreply.github.com>
2021-05-01 19:15:21 +0530

    Finding Maximum Number

---
+++ Maximum Of Two Numbers.py
@@ -0,0 +1,9 @@
+# Question: Write a Program to find out the maximum of two numbers as given by the user.
+
+num1=int(input('Input a number:'))
+num2=int(input('Input a number:'))
+
+if(num1>num2):
+    print('Maximum =',num1)
+else:
+    print('Maximum =',num2)
